738d51bb86951a9e114ac4b8cff726239e7efec6
jain-hemant <hemantjain1321@gmail.com>
2024-03-01 17:58:26 +0530

    Login: Modal- ongoing

--- src/modal/LoginModal.jsx
+++ src/modal/LoginModal.jsx
@@ -1,5 +1,6 @@
 import { Button, Flex, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function LoginModal({ isOpen, onClose }) {
     const initialRef = React.useRef(null)
@@ -28,6 +29,16 @@ export default function LoginModal({ isOpen, onClose }) {
                             {/* <FormLabel>Last name</FormLabel> */}
                             <Input type='password' placeholder='Password' />
                         </FormControl>
+                        <FormControl>
+                            <Flex>
+                                <Checkbox value=''>Keep me signed in </Checkbox>
+                                <Link>Forget password?</Link>
+                            </Flex>
+                        </FormControl>
+                        <Text>By clicking “Sign In”, you (1) agree to the current version of our <Link>TERMS OF USE</Link>, 
+                            and (2) have read Sephora’s <Link>Privacy Policy </Link>
+                        </Text>
+                        
                     </ModalBody>
 
                     <ModalFooter>
